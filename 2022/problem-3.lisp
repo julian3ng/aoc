c24b19de34fa9267021a2e9b63a50e991f3e6cea
julian3ng <julian3ng@gmail.com>
2022-12-03 01:22:42 -0500

    Introduce some utils, clean up

--- 2022/problem-3.lisp
+++ 2022/problem-3.lisp
@@ -11,30 +11,33 @@
   (gethash c *priorities* 0))
 
 
+(defun common-chars (s1 s2)
+  (intersection (coerce s1 'list) (coerce s2 'list)))
+
 (defun problem-3 ()
-  (let ((part1 (with-file-lines (lines "./input/3.txt" (lambda (line)
-                                                          (let ((l (/ (length line) 2)))
-                                                            (let ((sack1 (subseq line 0 l))
-                                                                  (sack2 (subseq line l)))
-                                                              (list sack1 sack2)))))
-                  (apply #'+  (mapcar (lambda (line) (destructuring-bind (s1 s2) line
-                                                  (let ((both (first (intersection (coerce  s1 'list) (coerce  s2 'list)))))
-                                                    (get-priority both))))
-                                      lines))))
+  (let ((part1
+          (with-file-lines
+              ;; Take each line and split it in half
+              (lines "./input/3.txt" (lambda (line)
+                                       (let ((l (/ (length line) 2)))
+                                         (let ((sack1 (subseq line 0 l))
+                                               (sack2 (subseq line l)))
+                                           (list sack1 sack2)))))
+
+            ;; Sum of priorities of the common char between halves
+            (sum (mapcar
+                  (lambda (line) (dbind (s1 s2) line (get-priority (first (common-chars s1 s2)))))
+                  lines))))
         (part2 (with-file-lines (lines "./input/3.txt")
-                 (let ((groups (loop for i from 0 to (/ (length lines) 3)
-                                      collect
-                                      (subseq lines (* i 3)
-                                              (let ((next (* (1+ i) 3)))
-                                                (unless (> next (length lines)) next))))))
-                   (apply #'+  (mapcar (lambda (group)
-                                         (get-priority  (when group
-                                                          (first (reduce
-                                                                  (lambda (g1 g2)
-                                                                    (format t "~A ~A~%" g1 g2)
-                                                                    (intersection (coerce g1 'list) (coerce g2 'list)))
-                                                                  group))))) groups))))))
-    part2))
+                 (let ((groups (group-list lines 3)))
+                   (sum
+                    ;; For each group
+                    (mapcar
+                     (lambda (group)
+                       ;; Get the priority of the common char between all
+                       ;; three elves in the group
+                       (get-priority (first (reduce #'common-chars group))))
+                     groups))))))
+    (format t "~&3-1: ~A~&3-2: ~A" part1 part2)))
 
 (problem-3)
-
--- 2022/utils.lisp
+++ 2022/utils.lisp
@@ -1,6 +1,12 @@
 (defpackage :utils
   (:use :cl :uiop :cl-ppcre)
-  (:export :with-file-lines :with-file-string))
+  (:export
+   :with-file-lines
+   :with-file-string
+   :dbind
+   :sum
+   :prod
+   :group-list))
 
 (in-package :utils)
 
@@ -14,3 +20,23 @@
 (defmacro with-file-string (varname filename &body body)
   `(let ((,varname (uiop:read-file-string ,filename)))
      ,@body))
+
+(defmacro dbind (lambda-list expression &body body)
+  `(destructuring-bind ,lambda-list ,expression ,@body)
+  )
+
+(defun sum (num-list)
+  (apply #'+ num-list))
+
+(defun prod (num-list)
+  (apply #'* num-list))
+
+
+(defun group-list (l n)
+  (let ((len (length l)))
+    (loop for i from 0 to (truncate len n)
+          for start = (* i n)
+          for end = (* (1+ i) n)
+          until (>= start len)
+          collect
+          (subseq l start (when (< end len) end)))))
